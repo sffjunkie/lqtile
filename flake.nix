52f65e38a4351d99a71745ce8a6dbd434062c9cd
Simon Kennedy <sffjunkie+code@gmail.com>
2025-09-25 11:06:14 +0100

    Move devshell to flake.nix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,22 @@
           pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-          default = import ./shell.nix { inherit pkgs; };
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs.buildPackages; [
+              uv
+              python3
+              python3.pkgs.myst-parser
+              python3.pkgs.pip
+              python3.pkgs.pipx
+              python3.pkgs.pytest
+              python3.pkgs.sphinx
+              python3.pkgs.sphinx-book-theme
+              python3.pkgs.tox
+              python3.pkgs.virtualenv
+              python3.pkgs.virtualenvwrapper
+              python3.pkgs.rich
+            ];
+          };
         }
       );
 
